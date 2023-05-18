8d4a762c68b8ffb77520de2239db0bf17c365d75
Markus Stoff <s52005@edu.campus02.at>
2023-05-18 19:33:06 +0200

    Cleaning up types and names in GameObjectPositioner
    - use 'gameObject' instead of 'tile'
    - use float as return type of translation functions

--- core/src/main/java/at/campus02/swd/game/gameobjects/GameObjectPositioner.java
+++ core/src/main/java/at/campus02/swd/game/gameobjects/GameObjectPositioner.java
@@ -6,27 +6,27 @@ package at.campus02.swd.game.gameobjects;
 public class GameObjectPositioner {
     private final int viewportWidth;
     private final int viewportHeight;
-    private final int tileSize;
+    private final int gameObjectSize;
 
-    public GameObjectPositioner(int viewportWidth, int viewportHeight, int tileSize) {
+    public GameObjectPositioner(int viewportWidth, int viewportHeight, int gameObjectSize) {
         this.viewportWidth = viewportWidth;
         this.viewportHeight = viewportHeight;
-        this.tileSize = tileSize;
+        this.gameObjectSize = gameObjectSize;
     }
 
-    public void setPosition(GameObject tile, int column, int line) {
-        tile.setPosition(translateColumnToX(column), translateLineToY(line));
+    public void setPosition(GameObject gameObject, int column, int line) {
+        gameObject.setPosition(translateColumnToX(column), translateLineToY(line));
     }
 
-    private int translateColumnToX(int column) {
-        int offsetX = viewportWidth / 2;
-        return column * tileSize - offsetX;
+    private float translateColumnToX(int column) {
+        float offsetX = viewportWidth / 2f;
+        return column * gameObjectSize - offsetX;
     }
 
-    private int translateLineToY(int line) {
-        int offsetY = viewportHeight / 2;
+    private float translateLineToY(int line) {
+        float offsetY = viewportHeight / 2f;
         // (line + 1) because we need to refer to the location of the *bottom* left corner of the image
         // return the negativ value because on the cartesian coordinate system our lines grow in the negative direction
-        return -((line + 1) * tileSize - offsetY);
+        return -((line + 1) * gameObjectSize - offsetY);
     }
 }
