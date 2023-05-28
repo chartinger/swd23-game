d389920a0e397eb1a5e464aeb3598023032c876e
Paul <00530734@edu.campus02.at>
2023-05-28 18:21:02 +0200

    observers log to log.txt

--- core/src/main/java/at/campus02/swd/game/observer/EnemyPositionObserver.java
+++ core/src/main/java/at/campus02/swd/game/observer/EnemyPositionObserver.java
@@ -1,10 +1,22 @@
 package at.campus02.swd.game.observer;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
 public class EnemyPositionObserver implements PositionObserver{
 
 
     @Override
     public void updatePosition(float x, float y, float rotation) {
-        System.out.println("x, y, rotation = " + x +", "+ y +", "+ rotation);
+        System.out.println(LocalDateTime.now()+" - Enemy at: x = "+ x + ", y = "+ y +", rot = "+ rotation +"^");
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("log.txt", true))) {
+            bw.write(LocalDateTime.now()+" - Enemy at: x = "+ x + ", y = "+ y +", rot = "+ rotation +"°");
+            bw.newLine();
+            bw.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
