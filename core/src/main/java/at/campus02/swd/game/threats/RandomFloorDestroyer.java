569d9f1e92b411e8a6bac9f22d8a2012108f5790
Markus Stoff <s52005@edu.campus02.at>
2023-06-10 10:05:18 +0200

    Fix: RandomFloorDestroyer may destroy to few tiles
    Since damage is not be dealt immediately on the game board, the
    random floor destroyer did not consider damage already dealt by
    itself within the round.

--- core/src/main/java/at/campus02/swd/game/threats/RandomFloorDestroyer.java
+++ core/src/main/java/at/campus02/swd/game/threats/RandomFloorDestroyer.java
@@ -5,9 +5,7 @@ import at.campus02.swd.game.board.BoardView;
 import at.campus02.swd.game.board.ThreatStrategy;
 import at.campus02.swd.game.util.Position;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class RandomFloorDestroyer implements ThreatStrategy {
     private final Random randomNumberGenerator = new Random();
@@ -19,18 +17,19 @@ public class RandomFloorDestroyer implements ThreatStrategy {
 
     @Override
     public List<Position> wreakHavoc(BoardView gameBoard) {
-        List<Position> damage = new ArrayList<>();
+        Set<Position> damage = new HashSet<>();
         for (int i = 0; i < tilesPerRound; i++)
-            damage.add(getRandomFloorToBeDestroyed(gameBoard));
-        return damage;
+            damage.add(getRandomFloorToBeDestroyed(gameBoard, damage));
+        damage.remove(null);
+        return List.copyOf(damage);
     }
 
-    private Position getRandomFloorToBeDestroyed(BoardView board) {
-        if (!hasDestructibleFields(board))
+    private Position getRandomFloorToBeDestroyed(BoardView board, Set<Position> damageDealt) {
+        if (!hasDestructibleFields(board, damageDealt))
             return null;
 
         Position position = getRandomPosition();
-        while (!isDestructible(board, position)) {
+        while (!isDestructible(board, damageDealt, position)) {
             position = getRandomPosition();
         }
 
@@ -43,17 +42,18 @@ public class RandomFloorDestroyer implements ThreatStrategy {
         return new Position(column, row);
     }
 
-    private boolean hasDestructibleFields(BoardView board) {
+    private boolean hasDestructibleFields(BoardView board, Set<Position> damageDealt) {
         for (int column = 0; column < Board.BOARD_WIDTH; column++)
             for (int row = 0; row < Board.BOARD_HEIGHT; row++)
-                if (isDestructible(board, new Position(column, row)))
+                if (isDestructible(board, damageDealt, new Position(column, row)))
                     return true;
         return false;
     }
 
-    private boolean isDestructible(BoardView board, Position position) {
+    private boolean isDestructible(BoardView board, Set<Position> damageDealt, Position position) {
         return !board.isPlayer(position)
             && !board.isFinish(position)
-            && !board.isDeadly(position);
+            && !board.isDeadly(position)
+            && !damageDealt.contains(position);
     }
 }
