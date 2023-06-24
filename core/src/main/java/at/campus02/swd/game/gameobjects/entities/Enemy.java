03643fd4a5466c38e25a48cad4c802bb094f0c0c
Treilind <thorsten.reisinger@gmx.net>
2023-06-24 13:21:46 +0200

    finalized strategy (enemy)

--- core/src/main/java/at/campus02/swd/game/gameobjects/entities/Enemy.java
+++ core/src/main/java/at/campus02/swd/game/gameobjects/entities/Enemy.java
@@ -2,20 +2,45 @@ package at.campus02.swd.game.gameobjects.entities;
 
 public class Enemy extends Entity{
 
+    private int stateOfStrategy;
+
     public Enemy(String spritePath){
         super(spritePath);
+        stateOfStrategy = 1;
+        super.speed = 2;
+
     }
 
     @Override
     public void update(){
 
-        if(super.getY() == 250) {
-            speed = -2;
+        if(stateOfStrategy == 1) {
 
-        } else if (super.getY() == -300) {
-            speed = 2;
+            if (super.getY() == 250) {
+                speed = -2;
+
+            } else if (super.getY() == -300) {
+                speed = 2;
+            }
+            super.setPosition(super.getX(), super.getY() + super.getSpeed());
+        } else if (stateOfStrategy == 2) {
+            if (super.getX() == 400) {
+                speed = -4;
+
+            } else if (super.getX() == 20) {
+                speed = 4;
+            }
+            super.setPosition(super.getX() + super.getSpeed(), super.getY());
+
         }
-        super.setPosition(super.getX(), super.getY() + super.getSpeed());
 
+    }
+
+    public int getStateOfStrategy() {
+        return stateOfStrategy;
+    }
+
+    public void setStateOfStrategy(int stateOfStrategy) {
+        this.stateOfStrategy = stateOfStrategy;
     }
 }
